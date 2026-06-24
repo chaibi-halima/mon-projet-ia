87463a5c99a517c28a23ef006cc00c07a8a2add2
Halima CHAIBI <chaibihalima2015@gmail.com>
2026-06-24 15:08:40 +0200

    ci: use explicit service autowire to fix prod container compilation

--- backend/src/State/ArticleAiProcessor.php
+++ backend/src/State/ArticleAiProcessor.php
@@ -2,12 +2,11 @@
 
 namespace App\State;
 
-use ApiPlatform\Doctrine\Common\State\PersistProcessor;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use App\Entity\Article;
 use App\Message\GenerateArticleMessage;
-use Symfony\Component\DependencyInjection\Attribute\Target;
+use Symfony\Component\DependencyInjection\Attribute\Autowire; // 🚀 1. On change l'import ici
 use Symfony\Component\Messenger\MessageBusInterface;
 
 /**
@@ -16,10 +15,12 @@ use Symfony\Component\Messenger\MessageBusInterface;
 class ArticleAiProcessor implements ProcessorInterface
 {
     public function __construct(
-        #[Target(PersistProcessor::class)]
+        // 🚀 2. On pointe directement sur le service ID exact d'API Platform Doctrine ORM
+        #[Autowire(service: 'api_platform.doctrine.orm.state.persist_processor')]
         private ProcessorInterface $persistProcessor,
-        private MessageBusInterface $messageBus, // 💡 On injecte le bus de messages
-    ) {}
+        private MessageBusInterface $messageBus,
+    ) {
+    }
 
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): mixed
     {
